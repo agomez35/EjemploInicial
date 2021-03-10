6f2d8cebe6e1fc66f792d6cc293dd1ccca6d3710
Andy Gómez De La Torre <andygomez35@gmail.com>
2021-03-10 15:56:23 -0500

    Mensaje en español

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Ejemplo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo!");
         }
     }
 }
