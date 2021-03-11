48a88173d2230393334e9aa56a68438a456d4dd9
Andy Gómez De La Torre <andygomez35@gmail.com>
2021-03-10 23:41:15 -0500

    De regreso mensaje en inglés

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Ejemplo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
